71f57791a7db2b69905443b2cd763f285150f743
Roe <bimo.17168@gmail.com>
2022-02-11 01:51:40 +0700

    Added MainMenuManager

---
+++ Assets/Script/MainMenuManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public Animator anim;
+    public Image image;
+    public void StartGame()
+    {
+        StartCoroutine(Fading());
+    }
+
+
+    IEnumerator Fading()
+    {
+        anim.SetBool("Fade", true);
+        yield return new WaitUntil(() => image.color.a == 1);
+        Application.LoadLevel("SampleScene");
+    }
+}
