592411900d6277ea09643b501b08608e9d8bb6cb
Roe <bimo.17168@gmail.com>
2022-02-10 17:02:46 +0700

    Added IEnumerator for Fade Func

--- Assets/Script/MainMenubtn.cs
+++ Assets/Script/MainMenubtn.cs
@@ -1,11 +1,21 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MainMenubtn : MonoBehaviour
 {
+    public Animator anim;
+    public Image image;
     public void StartGame()
     {
+        StartCoroutine(Fading());
+    }
+
+
+    IEnumerator Fading()
+    {
+        anim.SetBool("Fade", true);
+        yield return new WaitUntil(() => image.color.a == 1);
         Application.LoadLevel("SampleScene");
     }
 }
