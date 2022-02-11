f27e52693564b41f2249338cab1311ede59aced2
Roe <bimo.17168@gmail.com>
2022-02-12 02:19:50 +0700

    Update Lockpick script

--- Assets/Script/Lockpick.cs
+++ Assets/Script/Lockpick.cs
@@ -4,15 +4,20 @@ using UnityEngine;
 
 public class Lockpick : MonoBehaviour
 {
+    public Vector2 targetPosition;
+    public float speed;
+    private Vector2 position;
     // Start is called before the first frame update
     void Start()
     {
-        
+        position = gameObject.transform.position;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float moveSpeed = speed * Time.deltaTime;
+
+        transform.position = Vector2.MoveTowards(position, targetPosition, moveSpeed);
     }
 }
