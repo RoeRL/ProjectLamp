6d720b9d2917626a7b7350c824bef25625f94a7a
Roe <bimo.17168@gmail.com>
2022-02-14 23:40:58 +0700

    Added Southbtn Script

---
+++ Assets/Script/NEWSButton/Southbtn.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Southbtn : MonoBehaviour
+{
+    public SpriteRenderer spriteRenderer;
+    public static bool pressed = false;
+    private void LateUpdate()
+    {
+        if (pressed == false)
+        {
+            spriteRenderer.color = new Color(0, 0, 0, 1);
+        }
+    }
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player" && Keyboard.current.spaceKey.wasPressedThisFrame)
+        {
+            if (Eastbtn.pressed == true && Westbtn.pressed == true && Northbtn.pressed == true)
+            {
+                pressed = true;
+                spriteRenderer.color = new Color(0, 1, 0, 1);
+            }
+            else
+            {
+                pressed = false;
+                Eastbtn.pressed = false;
+                Westbtn.pressed = false;
+                Northbtn.pressed = false;
+                spriteRenderer.color = new Color(1, 0, 0, 1);
+            }
+        }
+    }
+}
