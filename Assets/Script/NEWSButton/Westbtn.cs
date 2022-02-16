f60fe951bb27efacadd9f19637c28058a1d11b59
Roe <bimo.17168@gmail.com>
2022-02-16 16:17:37 +0700

    Fix West Script

--- Assets/Script/NEWSButton/Westbtn.cs
+++ Assets/Script/NEWSButton/Westbtn.cs
@@ -3,9 +3,14 @@ using UnityEngine.InputSystem;
 
 public class Westbtn : MonoBehaviour
 {
-    public SpriteRenderer spriteRenderer;
+    public static SpriteRenderer spriteRenderer;
     public static bool pressed = false;
     private bool inside = false;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     private void Update()
     {
@@ -18,11 +23,7 @@ public class Westbtn : MonoBehaviour
             }
             else
             {
-                pressed = false;
-                Eastbtn.pressed = false;
-                Southbtn.pressed = false;
-                Northbtn.pressed = false;
-                spriteRenderer.color = new Color(1, 0, 0, 1);
+                StartCoroutine(ButtonPuzzleManager.ResetButton());
             }
         }
     }
@@ -31,12 +32,16 @@ public class Westbtn : MonoBehaviour
     {
         if (pressed == false)
         {
-            spriteRenderer.color = new Color(0, 0, 0, 1);
+            spriteRenderer.color = new Color(1, 1, 1, 1);
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         inside = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        inside = false;
     }
 }
