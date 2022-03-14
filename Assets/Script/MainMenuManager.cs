1eac92861835bf081039934624cde7ddd01dba1a
Roe <bimo.17168@gmail.com>
2022-03-14 15:42:24 +0700

    Add Interactable Script

--- Assets/Script/MainMenuManager.cs
+++ Assets/Script/MainMenuManager.cs
@@ -5,20 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
-    public Animator anim;
-    public Image image;
+
     
 
-    public void StartGame()
-    {
-        Time.timeScale = 1;
-        StartCoroutine(Fading());
-    }
+    
 
-    private IEnumerator Fading()
-    {
-        anim.SetBool("Fade", true);
-        yield return new WaitUntil(() => image.color.a == 1);
-        SceneManager.LoadScene("GUITest");
-    }
+    
 }
